d1755bde3ed40e71ecbb80dc6a821b042290015c
Nielson Trindade <nicoctrindade@gmail.com>
2015-06-27 11:23:19 -0300

    Test of updates for studies of language Matlab/Octave.

---
+++ matlab/Contents.m
@@ -0,0 +1,27 @@
+% Preferences.
+%
+% Saved preferences files.
+%   startup     - User startup M-file.
+%   finish      - User finish M-file.
+%   matlabrc    - Master startup M-file.
+%   pathdef     - Search path defaults.
+%   docopt      - Web browser defaults.
+%   printopt    - Printer defaults.
+%
+% Preference commands.
+%   cedit       - Set command line editor keys.
+%   terminal    - Set graphics terminal type.
+%   colordef    - Set color defaults.
+%   graymon     - Set graphics window defaults for gray-scale monitors.
+%   whitebg     - Change axes background color.
+%
+% Configuration information.
+%   hostid      - MATLAB server host identification number.
+%   license     - License number.
+%   version     - MATLAB version number.
+
+% Utilities.
+%   userpath    - User environment path.
+
+% Copyright (c) 1984-96 by The MathWorks, Inc.
+% $Revision: 1.9 $  $Date: 1996/10/29 03:24:56 $
